d3b70dac888930d21b24e8e26829dcbce14ffd2e
Ian Kronquist <iankronquist@teleport.com>
2014-11-08 12:34:53 -0800

    A better plan

--- problem1/freq.c
+++ problem1/freq.c
@@ -9,70 +9,68 @@ void filter_alpha_numeric(FILE *pipe_in, FILE *pipe_out);
 void filter_to_lower(FILE *pipe_in, FILE *pipe_out);
 void filter_split(FILE *pipe_in, FILE *pipe_out);
 void nop(FILE *pipe_in, FILE *pipe_out);
-void pipe_and_filter (FILE *pipe_in, FILE *pipe_out, int count,
-    const int num_filters, const void (**filter)(FILE*, FILE*));
+void pipe_and_filter (FILE *in_file, char ** exec_commands[], int num_commands,
+                      int cur_command);
 
 int main(int argc, char *argv[]) {
-    void (*filter_arr[])(FILE*, FILE*) = {nop, filter_alpha_numeric, filter_to_lower, filter_split};
-    int max = 4;
     FILE *in_file;
     if (argc > 1) {
         in_file = fopen(argv[1], "r");
     } else {
         in_file = stdin;
     }
-    fprintf(stdout, "test %p\n", stdout);
-    pipe_and_filter(in_file, stdout, 0, max, filter_arr);
+    char** commands[] = {
+        {"true", "\0"},
+        {"tr", "'[A-Z]'", "'[a-z]'", "\0"},
+        {"sed", "'s/[^a-zA-Z]/ /g'", "\0"}
+    };
+    pipe_and_filter(in_file, commands, 3, 0);
     return 0;
 }
 
-void pipe_and_filter (FILE *pipe_in, FILE *pipe_out, int count,
-    const int num_filters, const void (**filter)(FILE*, FILE*)) {
-    printf("Process: %d, pipes in %p out %p\n", count, pipe_in, pipe_out);
+void pipe_and_filter (FILE *in_file, char ** exec_commands[], int num_commands,
+                      int cur_command) {
     pid_t pid;
     int pipes_to_children[2]; // 0 is read 1 is write
-    FILE *child_pipe_read, *child_pipe_write;
-    if (pipe(pipes_to_children) != 0) {
+    int pipes_from_children[2]; // 0 is read 1 is write
+    if (pipe(pipes_to_children) != 0 && pipe(pipes_from_children) != 0) {
         printf("%s\n", errno == EFAULT ? "EFAULT" : "Not EFAULT");
         printf("%s\n", errno == EMFILE ? "EMFILE" : "Not EMFILE");
         assert(0);
     }
-    child_pipe_read = fdopen(pipes_to_children[0], "r");
-    child_pipe_write = fdopen(pipes_to_children[1], "w");
-    printf("Process: %d, pipes chin %p chout %p\n", count, child_pipe_read, child_pipe_write);
     pid = fork();
+    fprintf(stderr, "Fork pid %d\n", pid);
     if (pid == (pid_t)NULL) { // if this is the child process
-        fclose(child_pipe_write);
+        dup2(pipes_to_children[0], STDIN_FILENO);
+        dup2(pipes_from_children[1], STDOUT_FILENO);
         close(pipes_to_children[1]);
-        if (count < num_filters)
+        close(pipes_to_children[0]);
+        close(pipes_from_children[1]);
+        close(pipes_from_children[0]);
+        fprintf(stderr, "Running %s %d\n", exec_commands[cur_command], cur_command);
+        if (cur_command <  num_commands)
         {
-            count++;
-            pipe_and_filter(child_pipe_read, child_pipe_write,
-                count, num_filters, filter);
+            pipe_and_filter(pipes_to_children[0], exec_commands, num_commands, ++cur_command);
+            execvp(exec_commands[cur_command][0], &exec_commands[cur_command][1]);
         }
-        fclose(child_pipe_read);
-        close(pipes_to_children[0]);
     } else { // else this is the parent process
-        fclose(child_pipe_read);
-        close(pipes_to_children[0]);
-        filter[count](child_pipe_write, child_pipe_read);
-        while (!feof(pipe_in)) {
-            printf("pid %d put %d\n", pid, count);
-            char c = fgetc(pipe_in);
-            printf("!!Process: %d, pipes in %p out %p, %c\n", count, pipe_in, pipe_out, c);
-            fputc(c, child_pipe_write);
+        FILE *from_ch_stream = fdopen(pipes_from_children[0], "r");
+        FILE *to_ch_stream = fdopen(pipes_to_children[1], "w");
+        if (cur_command < num_commands) {
+            while (!feof(from_ch_stream)) {
+               fputc(fgetc(from_ch_stream), to_ch_stream);
+               fputc(fgetc(from_ch_stream), stderr);
+            }
         }
-        /*while (!feof(child_pipe_read)) {
-            char c = fgetc(child_pipe_read);
-            printf("!!Process: %d, pipes in %p out %p, %c\n", count, pipe_in, pipe_out, c);
-            fputc(c, pipe_out);
-        }*/
-        kill(pid, SIGTERM);
+        fclose(from_ch_stream);
+        fclose(to_ch_stream);
         close(pipes_to_children[1]);
-        filter[count](child_pipe_read, pipe_out);
+        close(pipes_to_children[0]);
+        close(pipes_from_children[1]);
+        close(pipes_from_children[0]);
+        kill(pid, SIGTERM);
     }
-    printf("Process %d\n", count);
-    printf("Exit pid %d\n", pid);
+    fprintf(stderr, "Exit pid %d\n", pid);
     exit(0);
 }
 
