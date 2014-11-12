06669b7ad3ecc1b173fbc5f195f4e71b06fe1edc
Ian Kronquist <iankronquist@teleport.com>
2014-11-11 21:25:30 -0800

    The dman thing works

--- problem1/freq.c
+++ problem1/freq.c
@@ -11,98 +11,107 @@ void filter_split(FILE *pipe_in, FILE *pipe_out);
 void nop(FILE *pipe_in, FILE *pipe_out);
 void pipe_and_filter (FILE *in_file, char ** exec_commands[], int num_commands,
                       int cur_command);
+void only_sed();
 
-int main(int argc, char *argv[]) {
-    FILE *in_file;
-    if (argc > 1) {
-        in_file = fopen(argv[1], "r");
-    } else {
-        in_file = stdin;
+int main(int argc, char *argv[]) { // read is 0 write is 1
+
+    int in_to_sed[2];
+    int sed_to_tr[2];
+    int tr_to_awk[2];
+    int awk_to_sort[2];
+    int sort_to_uniq[2];
+    int uniq_out[2];
+    if (pipe(in_to_sed) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
     }
-    char** commands[] = {
-        {"true", "\0"},
-        {"tr", "'[A-Z]'", "'[a-z]'", "\0"},
-        {"sed", "'s/[^a-zA-Z]/ /g'", "\0"}
-    };
-    pipe_and_filter(in_file, commands, 3, 0);
+    if (pipe(sed_to_tr) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
+    }
+    if (pipe(tr_to_awk) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
+    }    if (pipe(awk_to_sort) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
+    }    if (pipe(sort_to_uniq) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
+    }    if (pipe(uniq_out) == -1) {
+        perror("Couldn't opend pipes!");
+        exit(-1);
+    }
+    switch (fork()) {
+        case -1:
+            perror("Couldn't fork properly");
+            exit(-1);
+        case 0: //child
+            dup2(sed_to_tr[1], STDOUT_FILENO);
+            close(sed_to_tr[0]);
+            close(sed_to_tr[1]);
+            execlp("sed", "sed", "s/[^a-zA-Z]/ /g", NULL);
+        default:
+            close(sed_to_tr[1]);
+            break;
+    }
+    switch(fork()) {
+        case -1:
+            perror("Couldn't fork properly");
+            exit(-1);
+        case 0:
+            dup2(sed_to_tr[0], STDIN_FILENO);
+            dup2(tr_to_awk[1], STDOUT_FILENO);
+            close(sed_to_tr[0]);
+            close(tr_to_awk[1]);
+            execlp("tr", "tr", "[A-Z]", "[a-z]", NULL);
+        default:
+            close(sed_to_tr[0]);
+            close(tr_to_awk[1]);
+            break;
+    }
+    switch (fork()) {
+        case -1:
+            perror("Couldn't fork properly");
+            exit(-1);
+        case 0: //child
+            dup2(tr_to_awk[0], STDIN_FILENO);
+            dup2(awk_to_sort[1], STDOUT_FILENO);
+            close(tr_to_awk[0]);
+            close(awk_to_sort[1]);
+            execlp("awk", "awk", "{ for(i = 1; i <= NF; i++) { print $i; } }", NULL);
+        default:
+            close(tr_to_awk[0]);
+            close(awk_to_sort[1]);
+            break;
+    }
+    switch (fork()) {
+        case -1:
+            perror("Couldn't fork properly");
+            exit(-1);
+        case 0: //child
+            dup2(awk_to_sort[0], STDIN_FILENO);
+            dup2(sort_to_uniq[1], STDOUT_FILENO);
+            close(awk_to_sort[0]);
+            close(sort_to_uniq[1]);
+            execlp("sort", "sort", NULL);
+        default:
+            close(awk_to_sort[0]);
+            close(sort_to_uniq[1]);
+            break;
+    }
+    switch (fork()) {
+        case -1:
+            perror("Couldn't fork properly");
+            exit(-1);
+        case 0: //child
+            dup2(sort_to_uniq[0], STDIN_FILENO);
+            close(sort_to_uniq[0]);
+            execlp("uniq", "uniq", "-c", NULL);
+        default:
+            close(sort_to_uniq[0]);
+            break;
+    }
+
     return 0;
-}
-
-void pipe_and_filter (FILE *in_file, char ** exec_commands[], int num_commands,
-                      int cur_command) {
-    pid_t pid;
-    int pipes_to_children[2]; // 0 is read 1 is write
-    int pipes_from_children[2]; // 0 is read 1 is write
-    if (pipe(pipes_to_children) != 0 && pipe(pipes_from_children) != 0) {
-        printf("%s\n", errno == EFAULT ? "EFAULT" : "Not EFAULT");
-        printf("%s\n", errno == EMFILE ? "EMFILE" : "Not EMFILE");
-        assert(0);
-    }
-    pid = fork();
-    fprintf(stderr, "Fork pid %d\n", pid);
-    if (pid == (pid_t)NULL) { // if this is the child process
-        dup2(pipes_to_children[0], STDIN_FILENO);
-        dup2(pipes_from_children[1], STDOUT_FILENO);
-        close(pipes_to_children[1]);
-        close(pipes_to_children[0]);
-        close(pipes_from_children[1]);
-        close(pipes_from_children[0]);
-        fprintf(stderr, "Running %s %d\n", exec_commands[cur_command], cur_command);
-        if (cur_command <  num_commands)
-        {
-            pipe_and_filter(pipes_to_children[0], exec_commands, num_commands, ++cur_command);
-            execvp(exec_commands[cur_command][0], &exec_commands[cur_command][1]);
-        }
-    } else { // else this is the parent process
-        FILE *from_ch_stream = fdopen(pipes_from_children[0], "r");
-        FILE *to_ch_stream = fdopen(pipes_to_children[1], "w");
-        if (cur_command < num_commands) {
-            while (!feof(from_ch_stream)) {
-               fputc(fgetc(from_ch_stream), to_ch_stream);
-               fputc(fgetc(from_ch_stream), stderr);
-            }
-        }
-        fclose(from_ch_stream);
-        fclose(to_ch_stream);
-        close(pipes_to_children[1]);
-        close(pipes_to_children[0]);
-        close(pipes_from_children[1]);
-        close(pipes_from_children[0]);
-        kill(pid, SIGTERM);
-    }
-    fprintf(stderr, "Exit pid %d\n", pid);
-    exit(0);
-}
-
-void nop(FILE *pipe_in, FILE *pipe_out){ puts("nop");}
-
-void filter_alpha_numeric(FILE *pipe_in, FILE *pipe_out) {
-    char c;
-    while(!feof(pipe_in)) {
-        c = fgetc(pipe_in);
-        if (isalnum(c)) {
-            fputc(c, pipe_out);
-            printf("%c", c);
-        }
-    }
-}
-
-void filter_to_lower(FILE *pipe_in, FILE *pipe_out) {
-    char c;
-    while(!feof(pipe_in)) {
-        c = fgetc(pipe_in);
-        printf("%c", c);
-        fputc(tolower(c), pipe_out);
-    }
-}
-
-void filter_split(FILE *pipe_in, FILE *pipe_out) {
-    char c;
-    while(!feof(pipe_in)) {
-        c = fgetc(pipe_in);
-        printf("%c", c);
-        if (c == ' ') {
-            fputc('\n', pipe_out);
-        }
-    }
 }
